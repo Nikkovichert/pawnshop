a2e9b527f0a7ceeef2629fd2864309b6f7210c6d
Nikko <nikkovichert@gmail.com>
2018-05-01 07:33:50 +0000

    Queen valid move

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,8 +1,13 @@
 class Queen < Piece
 
     def valid_move?(new_x, new_y)
-        return false if square_occupied?(x_position, y_position)
-        return false if self.is_obstructed?(x_position, y_position)
-        horizontal_move?(x_position, y_position) ||  vertical_move?(x_position, y_position) ||  diagonal_move?(x_position, y_position)
+      x1 = self.x_position
+      y1 = self.y_position
+      own_piece_at_destination?(new_x, new_y)
+      !is_obstructed?(new_x, new_y)
+      !off_board?(new_x new_y)
+      ((x_position != new_x && y_position != new_y) && ((x1 + new_x).abs === (y1 + new_y).abs)) ||
+      ((y_position == new_y) && (x_position != new_x)) || ((x_position == new_x) && (y_position !=new_y))
     end
+
 end
