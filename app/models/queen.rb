8f0f727168ec7f72f5aea90fe98571481d8529bc
Nikko <nikkovichert@gmail.com>
2018-05-01 23:52:19 +0000

    fixed lack of &&s in queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -3,11 +3,11 @@ class Queen < Piece
     def valid_move?(new_x, new_y)
       x1 = self.x_position
       y1 = self.y_position
-      own_piece_at_destination?(new_x, new_y)
-      !is_obstructed?(new_x, new_y)
-      !off_board?(new_x new_y)
-      ((x_position != new_x && y_position != new_y) && ((x1 + new_x).abs === (y1 + new_y).abs)) ||
-      ((y_position == new_y) && (x_position != new_x)) || ((x_position == new_x) && (y_position !=new_y))
+      own_piece_at_destination?(new_x, new_y) &&
+      !is_obstructed?(new_x, new_y) &&
+      !off_board?(new_x new_y) &&
+      (((x_position != new_x && y_position != new_y) && ((x1 + new_x).abs === (y1 + new_y).abs)) ||
+      ((y_position == new_y) && (x_position != new_x)) || ((x_position == new_x) && (y_position !=new_y)))
     end
 
 end
